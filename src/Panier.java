adecbfca4716c667b5ec05d24c0c295437054b9b
mariam mahamat soraya <mariammahamat557@gmail.com>
2024-02-23 16:45:20 +0100

    premier commit

---
+++ src/Panier.java
@@ -0,0 +1,19 @@
+public class Panier {import java.util.ArrayList;
+
+    public class Panier {
+        private ArrayList<Produit> articles = new ArrayList<>();
+
+        public void addProduit(Produit produit){
+            articles.add(produit);
+        }
+
+        public  int prixTotalPanier(){
+            int prixTotal = 0;
+            for (Produit produit : articles){
+                prixTotal += produit.getPrix();
+            }
+            return prixTotal;
+        }
+
+    }
+
